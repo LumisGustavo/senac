5b57073fd560fa06ad31a08ede89b8e90ce273e6
Luiz Gustavo <luiz.gustavo.corp@gmail.com>
2025-04-08 17:47:18 -0300

    Add files via upload

---
+++ Python/aula01.py
@@ -0,0 +1,16 @@
+'''
+Inteiros= int
+Palavras/Letras= string
+Fracionado/Flutuante= float
+'''
+
+def bom_dia():
+    print("Hello World!")
+
+bom_dia()
+
+def saudacao(nome):
+    print(f"Olá, {nome}! Seja Bem-Vindo ao mundo do Python")
+
+a = "Gustavo"
+saudacao(a)
