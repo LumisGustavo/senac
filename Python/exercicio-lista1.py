c33ebb256cf6cfe7b41ed032117912e20a943ac2
Luiz Gustavo <luiz.gustavo.corp@gmail.com>
2025-04-10 17:44:52 -0300

    Update exercicio-lista1.py

--- Python/exercicio-lista1.py
+++ Python/exercicio-lista1.py
@@ -28,4 +28,34 @@ desconto()
 """Avaliação de Nota Escolar"""
 def nota():
     print("Avaliação de Boletim")
-    avaliacao= int (input("Digite sua nota: "))
+    avaliacao = float (input("Digite sua nota: "))
+
+    if avaliacao >= 7:
+        print("Você foi aprovado")
+
+    elif avaliacao >= 5 <=6.9:
+        print("Você está de recuperação")
+
+    else:
+        print("Você foi reprovado")
+
+nota()
+
+"""Calculadora de Horas Trabalhadas"""
+def trabalho():
+    print("Calcule suas Horas Trabalhadas")
+    print("Valor da hora: R$6.30")
+    horas= float(input("Digite suas horas de jornada: "))
+
+    horacalculada = (horas) * 6.3
+    horaextra1 = 50.4
+    horaextra2 = (horas) - 8
+
+    if horacalculada <= 8:
+        print(f"Você recebeu: R${horacalculada}")
+
+    elif horacalculada >= 8.1:
+        print(f"Você recebeu: R${horaextra1}")
+        print(f"Suas horas extras {horaextra2} estão contabilizadas no banco de dados")
+
+trabalho()
