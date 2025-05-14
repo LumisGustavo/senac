ba1563f8ef1af884ffddf4c65ae52bf9dd0df3f2
Luiz Gustavo <luiz.gustavo.corp@gmail.com>
2025-05-14 17:40:10 -0300

    Update app.py

--- Projeto-Completo/app.py
+++ Projeto-Completo/app.py
@@ -9,7 +9,7 @@ from werkzeug.utils import secure_filename # Biblioteca que garante nomes seguro
 app = Flask(__name__) # Criação da aplicação Flask
 app.config['SECRET_KEY'] = 'chave_mauri' # Chave secreta utilizada nas sessões
 app.config['UPLOAD_FOLDER'] = 'static/uploads' # Pasta para onde imagens serão salvas
-app.config['MAX_CONTENT_LENGH'] = 2 * 1024 * 1024 # Limite do tamanho de uploads para 2mb
+app.config['MAX_CONTENT_LENGTH'] = 2 * 1024 * 1024 # Limite do tamanho de uploads para 2mb
 
 EXTENSOES = {'png', 'jpg', 'jpeg', 'gif'} #Extensões permitidas
 
@@ -61,7 +61,7 @@ def inicializar_banco():
                    conteudo TEXT NOT NULL,
                    imagem TEXT,
                    autor_id INTEGER NOT NULL,
-                   FOREING KEY (autor_id) REFERENCES usuarios (id)
+                   FOREIGN KEY (autor_id) REFERENCES usuarios (id)
                 );
             ''')
         db.commit()
@@ -76,8 +76,95 @@ def index():
         SELECT p.titulo, p.conteudo, p.imagem, u.nome
         FROM posts p
         JOIN usuarios u ON p.autor_id = u.id
-''')
+    ''').fetchall()
+    return render_template('index.html', posts=posts)
 
 # --------------------------------------------------------- Rota Registro de Usuário (register) ---------------------------------------------------------
 
 @app.route('/register', methods=['GET', 'POST'])
+def register():
+    # Exibir o formulário e processar os dados enviados.
+    if request.method == 'POST':
+        nome = request.form['nome']
+        cpf = request.form['cpf']
+        email = request.form['email']
+        senha = request.form['senha']
+
+        # Validar se a senha digitada possui no mínimo 8 caracteres, 1 maiuscula, 1 número e 1 símbolo
+        if len(senha) < 8:
+            return "Senha fraca. Requisitos: 8+ caracteres, 1 maiuscula, 1 número e 1 símbolo."
+        
+        db = get_db()
+        try:
+            db.execute('INSERT INTO usuarios (nome, cpf, email, senha) VALUES (?, ?, ?, ?)', (nome, cpf, email, senha))
+            db.commit()
+            return redirect(url_for('login'))
+        except sqlite3.IntegrityError:
+            return "Error: CPF ou email já cadastrados."
+    
+    return render_template('register.html')
+
+# --------------------------------------------------------- Rota de Login (login) ---------------------------------------------------------
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    # Exibir e processar o formulário de login
+    if request.method == 'POST':
+        email = request.form['email']
+        senha = request.form['senha']
+        db = get_db()
+        usuario = db.execute('SELECT * FROM usuarios WHERE email=? AND senha=?', (email, senha)).fetchone()
+        if usuario:
+            session['usuario_id'] = usuario['id']
+            session['usuario_nome'] = usuario['nome']
+            return redirect(url_for('dashboard'))
+        else:
+            return "Login inválido."
+    return render_template('login.html')
+
+# --------------------------------------------------------- Painel do Usuário ---------------------------------------------------------
+@app.route('/dashboard')
+def dashboard():
+# Exibir os posts do usuário logado.
+    if 'usuario_id' not in session:
+        return redirect(url_for('login'))
+    
+    db = get_db()
+    posts = db.execute('SELECT * FROM posts WHERE autor_id=?', (session['usuario_id'],)).fetchall()
+    return render_template('dashboard.html', posts=posts)
+
+# --------------------------------------------------------- Rota para criar novo post ---------------------------------------------------------
+@app.route('/new_post', methods=['GET', 'POST'])
+def new_post():
+    #Permitir que o usuário logado crie um novo post com ou sem imagem
+    if 'usuario_id' not in session:
+        return redirect(url_for('login'))
+    
+    if request.method == 'POST':
+        titulo = request.form['titulo']
+        conteudo = request.form['conteudo']
+        imagem = request.form['imagem']
+
+        nome_arquivo = None
+        if imagem and extensao_valida(imagem.filename):
+            nome_arquivo = secure_filename(imagem.filename)
+            imagem.save(os.path.join(app.config['UPLOAD_FOLDER'], nome_arquivo))
+
+        db = get_db()
+        db.execute('INSERT INTO posts (titulo, conteudo, imagem, autor_id) VALUES (?, ?, ?, ?)', (titulo, conteudo, nome_arquivo, session['usuario_id']))
+        db.commit()
+        return redirect(url_for('dashboard'))
+
+    return render_template('new_post.html')
+    
+# --------------------------------------------------------- Rota para logout ---------------------------------------------------------
+@app.route('/logout')
+def logout():
+    # Remove o usuário da sessão atual.
+    session.clear()
+    return redirect(url_for('index'))
+
+# --------------------------------------------------------- Execução principal ---------------------------------------------------------
+if __name__ == '__main__':
+    os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True) # Cria a pasta de uploads se ela não existir
+    inicializar_banco() # Garante que o banco e tabelas sejam criados
+    app.run(debug=True)
